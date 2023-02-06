a6b307d994b437785e728376821339a79ac027a6
SandeepTheDev <124437404+SandeepTheDev@users.noreply.github.com>
2023-02-06 17:58:08 +0530

    added react query for fetching pet details

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,12 +1,22 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Details from "./Details.jsx";
 import SearchParams from "./SearchParams.jsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
+      <QueryClientProvider client={queryClient}>
         <header>
           <Link to="/">Adopt Me</Link>
         </header>
@@ -14,7 +24,7 @@ const App = () => {
           <Route path="/details/:id" element={<Details />} />
           <Route path="/" element={<SearchParams />} />
         </Routes>
-      </div>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
